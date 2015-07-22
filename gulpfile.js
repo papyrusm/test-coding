b6f6a6fe6547e1cc6920c5ab2db18b51a693e811
pavel <testmail@yandex.ru>
2015-07-22 11:03:47 +0400

    My template

---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+var gulp = require('gulp'),
+  connect = require('gulp-connect'),
+  opn = require('opn');
+
+//запускаем локальный	 сервер
+gulp.task('connect', function() {
+  connect.server({
+    root: 'app',
+    livereload: true,
+    port: 8888 
+  });
+  opn('http://localhost:8888');
+});
+
+//Работа с HTML
+gulp.task('html', function () {
+  gulp.src('./app/*.html')
+    .pipe(connect.reload());
+});
+
+//Работа с CSS
+gulp.task('css', function () {
+  gulp.src('./app/css/*.css')
+    .pipe(connect.reload());
+});
+//Работа с js
+gulp.task('js', function () {
+  gulp.src('./app/js/*.js')
+    .pipe(connect.reload());
+});
+
+//Слежка
+gulp.task('watch', function () {
+  gulp.watch(['./app/*.html'], ['html']);
+  gulp.watch(['./app/css/*.css'], ['css']);
+  gulp.watch(['./app/js/*.js'], ['js']);
+});
+
+//дефолт задача
+gulp.task('default', ['connect', 'watch'])
